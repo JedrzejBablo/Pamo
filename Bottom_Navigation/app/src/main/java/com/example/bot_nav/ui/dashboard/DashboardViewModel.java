b2edda32823e340e8c65bc419a8cfa04d841c8e4
JedrzejBablo <jedrzej_bablo@wp.pl>
2019-10-14 17:59:53 +0200

    botom_navigation

---
+++ Bottom_Navigation/app/src/main/java/com/example/bot_nav/ui/dashboard/DashboardViewModel.java
@@ -0,0 +1,19 @@
+package com.example.bot_nav.ui.dashboard;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class DashboardViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public DashboardViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("TEST");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
