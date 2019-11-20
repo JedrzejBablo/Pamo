910cf1271418a9ce23e885ee29e6209f4acea15f
JedrzejBablo <jedrzej_bablo@wp.pl>
2019-11-20 19:22:02 +0100

    add quiz and chart

--- Bot_nav_app(3.4)/app/src/main/java/com/example/exercise_2/MainActivity.java
+++ Bot_nav_app(3.4)/app/src/main/java/com/example/exercise_2/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.exercise_2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
@@ -7,10 +8,12 @@ import android.support.annotation.NonNull;
 import android.text.TextUtils;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RadioGroup;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import androidx.constraintlayout.widget.ConstraintLayout;
@@ -20,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     private LinearLayout home;
     private LinearLayout dashboard;
     private LinearLayout notifications;
+    private ScrollView scrollView;
     public ConstraintLayout images;
     public ImageView very_low;
     public ImageView low;
@@ -28,6 +32,11 @@ public class MainActivity extends AppCompatActivity {
     public int gender = 1;
     private double ppm = 0.0;
     public RadioGroup radioGroup;
+
+    private Button  activityChanger, quizButton;
+    private EditText q1, q2, q3, q4;
+    private TextView welcomeMessage;
+
 
 
 
@@ -38,11 +47,11 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
                     home.findViewById(R.id.home_layout).setVisibility(View.VISIBLE);
                     dashboard.findViewById(R.id.dashboard_layout).setVisibility(View.INVISIBLE);
                     notifications.findViewById(R.id.notifications_layout).setVisibility(View.INVISIBLE);
                     images.findViewById(R.id.images_layout).setVisibility(View.INVISIBLE);
+                    scrollView.findViewById(R.id.scrollView).setVisibility(View.INVISIBLE);
                     return true;
                 case R.id.navigation_dashboard:
                     //mTextMessage.setText(R.string.title_dashboard);
@@ -50,45 +59,18 @@ public class MainActivity extends AppCompatActivity {
                     dashboard.findViewById(R.id.dashboard_layout).setVisibility(View.VISIBLE);
                     notifications.findViewById(R.id.notifications_layout).setVisibility(View.INVISIBLE);
                     images.findViewById(R.id.images_layout).setVisibility(View.INVISIBLE);
-                    final EditText bmi_waga = (EditText) findViewById(R.id.bmi_wagaEdit);
-                    final EditText bmi_wzrost = (EditText) findViewById(R.id.bmi_wzrostEdit);
-                    final TextView bmi_wynik = (TextView) findViewById(R.id.bmiWynik);
-                    findViewById(R.id.bmi_oblicz).setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            String str1 = bmi_waga.getText().toString();
-                            String str2 = bmi_wzrost.getText().toString();
+                    scrollView.findViewById(R.id.scrollView).setVisibility(View.INVISIBLE);
 
-                            if (TextUtils.isEmpty(str1)) {
-                                bmi_waga.setError("Prosze podac wage");
-                                bmi_waga.requestFocus();
-                                return;
-                            }
-
-                            if (TextUtils.isEmpty(str2)) {
-                                bmi_wzrost.setError("Prosze podac wzrost");
-                                bmi_wzrost.requestFocus();
-                                return;
-                            }
-
-                            float bmi_waga = Float.parseFloat(str1);
-                            float bmi_wzrost = Float.parseFloat(str2) / 100;
-
-                            float bmiWartosc = obliczBMI(bmi_waga, bmi_wzrost);
-
-                            String wynikBMI = wynikBMI(bmiWartosc);
-
-                            bmi_wynik.setText(String.valueOf(bmiWartosc + "-" + wynikBMI));
-                        }
-                    });
+                    findViewById(R.id.bt_activity_changer);
                     return true;
 
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    //mTextMessage.setText(R.string.title_notifications);
                     home.findViewById(R.id.home_layout).setVisibility(View.INVISIBLE);
                     dashboard.findViewById(R.id.dashboard_layout).setVisibility(View.INVISIBLE);
                     notifications.findViewById(R.id.notifications_layout).setVisibility(View.VISIBLE);
                     images.findViewById(R.id.images_layout).setVisibility(View.VISIBLE);
+                    scrollView.findViewById(R.id.scrollView).setVisibility(View.VISIBLE);
 
                     radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
                     radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
@@ -111,6 +93,7 @@ public class MainActivity extends AppCompatActivity {
                     final EditText ppm_wzrost = (EditText) findViewById(R.id.ppm_wzrostEdit);
                     final EditText ppm_wiek = (EditText) findViewById(R.id.ppm_wiekEdit);
                     final TextView ppm_wynik = (TextView) findViewById(R.id.ppm_wynik);
+                    final TextView bmi_wynik = (TextView) findViewById(R.id.bmiWynik);
 
                     findViewById(R.id.ppm_oblicz).setOnClickListener(new View.OnClickListener() {
                         @Override
@@ -139,6 +122,7 @@ public class MainActivity extends AppCompatActivity {
 
                             float ppm_waga = Float.parseFloat(str1);
                             float ppm_wzrost = Float.parseFloat(str2);
+                            float ppm_wzrost2 = ppm_wzrost / 100;
                             float ppm_wiek = Float.parseFloat(str3);
 
                             double ppmWartosc = obliczPPM(ppm_waga, ppm_wzrost, ppm_wiek);
@@ -146,6 +130,12 @@ public class MainActivity extends AppCompatActivity {
                             String wynikPPM = wynikPPM(ppmWartosc);
 
                             ppm_wynik.setText(String.valueOf(ppmWartosc + " kcal" + wynikPPM));
+
+                            float bmiWartosc = obliczBMI(ppm_waga, ppm_wzrost2);
+
+                            String wynikBMI = wynikBMI(bmiWartosc);
+
+                            bmi_wynik.setText(String.valueOf(bmiWartosc + "-" + wynikBMI));
                         }
                     });
 
@@ -161,20 +151,63 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        mTextMessage = findViewById(R.id.Home);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
         home = findViewById(R.id.home_layout);
         dashboard = findViewById(R.id.dashboard_layout);
         notifications = findViewById(R.id.notifications_layout);
         images = findViewById(R.id.images_layout);
+        scrollView = findViewById(R.id.scrollView);
         home.findViewById(R.id.home_layout).setVisibility(View.VISIBLE);
         dashboard.findViewById(R.id.dashboard_layout).setVisibility(View.INVISIBLE);
         notifications.findViewById(R.id.notifications_layout).setVisibility(View.INVISIBLE);
         images.findViewById(R.id.images_layout).setVisibility(View.INVISIBLE);
+        scrollView.findViewById(R.id.scrollView).setVisibility(View.INVISIBLE);
+        welcomeMessage = findViewById(R.id.welcome_message_textView);
+        activityChanger = findViewById(R.id.bt_activity_changer);
+        quizButton = findViewById(R.id.bt_quiz);
+        q1 = findViewById(R.id.styczen_text_id);
+        q2 = findViewById(R.id.luty_text_id);
+        q3 = findViewById(R.id.marzec_text_id);
+        q4 = findViewById(R.id.kwiecien_text_id);
+
+
+
+        activityChanger.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                launchActivity();
+            }
+        });
+
+        quizButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startQuiz();
+            }
+        });
+
     }
 
-    private float obliczBMI (float bmi_waga, float bmi_wzrost) {
-        return (float) (bmi_waga / (bmi_wzrost * bmi_wzrost));
+    private void launchActivity() {
+
+        Intent intent = new Intent(this, ChartActivity.class);
+        intent.putExtra("q1", q1.getText().toString());
+        intent.putExtra("q2", q2.getText().toString());
+        intent.putExtra("q3", q3.getText().toString());
+        intent.putExtra("q4", q4.getText().toString());
+        startActivity(intent);
+    }
+
+    private void startQuiz() {
+        Intent intent = new Intent(this, QuizActivity.class);
+        startActivity(intent);
+    }
+
+    private float obliczBMI (float ppm_waga, float ppm_wzrost2) {
+        return (float) (ppm_waga / (ppm_wzrost2 * ppm_wzrost2));
     }
 
     private String wynikBMI(float bmiWartosc) {
